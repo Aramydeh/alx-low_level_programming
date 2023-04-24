5b5eb226e606f3c7bafb18399bc312db22ce19a0
Gbolahan Aramide <aramydeh@gmail.com>
2023-04-24 15:45:36 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,41 +1,20 @@
-#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
- * main - prints all possible different combinations of two two-digit
- * base-10 numbers
+ * main - Prints a string with printf
  *
- * Return: 0 if execution was successful
+ * Return: 0 if program executed successfully
  */
 int main(void)
 {
-	int num1;
-	int num2;
-	int upper_limit;
+	int stdErrFileNo = (2);
+	char buf[60];
+	size_t nBytes;
 
-	upper_limit = 99;
-
-	for (num1 = 0; num1 <= upper_limit; num1++)
-	{
-		for (num2 = 0; num2 <= upper_limit; num2++)
-		{
-			if (num1 < num2)
-			{
-				if (num1 >= 0 && num2 > 1)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-
-				putchar((num1 / 10) + 0x30);
-				putchar((num1 % 10) + 0x30);
-				putchar(' ');
-				putchar((num2 / 10) + 0x30);
-				putchar((num2 % 10) + 0x30);
-			}
-		}
-	}
-
-	putchar('\n');
-	return (0);
+	strcpy(buf, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	nBytes = strlen(buf);
+	write(stdErrFileNo, buf, nBytes);
+	return (1);
 }
 
