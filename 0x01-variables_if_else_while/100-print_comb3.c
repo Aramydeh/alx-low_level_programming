b21418dda6826c59a671a1e9456886760cbc21f8
Gbolahan Aramide <aramydeh@gmail.com>
2023-04-24 16:31:06 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- * main - prints all possible different combinations of three base-10 digits
+ * main - prints all possible different combinations of two base-10 digits
  *
  * Return: 0 if execution was successful
  */
@@ -10,30 +9,20 @@ int main(void)
 {
 	int digit1;
 	int digit2;
-	int digit3;
 
 	for (digit1 = 0x30; digit1 <= 0x39; digit1++)
 	{
 		for (digit2 = 0x30; digit2 <= 0x39; digit2++)
 		{
-			for (digit3 = 0x30; digit3 <= 0x39; digit3++)
+			if (digit1 < digit2)
 			{
-				if (digit1 < digit2 && digit2 < digit3)
+				putchar(digit1);
+				putchar(digit2);
+
+				if ((digit1 >= 0x30 && digit1 < 0x38) && digit2 <= 0x39)
 				{
-					int req1;
-					int req2;
-
-					putchar(digit1);
-					putchar(digit2);
-					putchar(digit3);
-					req1 = digit1 >= 0x30 && digit1 < 0x37;
-					req2 = digit2 >= 0x30 && digit2 <= 0x38;
-
-					if (req1 && req2 && digit3 <= 0x39)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
@@ -42,3 +31,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
