24b5a511b93af19b0850450df3ac0f613afd9238
Gbolahan Aramide <aramydeh@gmail.com>
2023-04-24 16:16:39 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,20 +1,45 @@
+#include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 
 /**
- * main - Prints a string with printf
+ * main - prints all possible different combinations of three base-10 digits
  *
- * Return: 0 if program executed successfully
+ * Return: 0 if execution was successful
  */
 int main(void)
 {
-	int stdErrFileNo = (2);
-	char buf[60];
-	size_t nBytes;
+	int digit1;
+	int digit2;
+	int digit3;
 
-	strcpy(buf, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	nBytes = strlen(buf);
-	write(stdErrFileNo, buf, nBytes);
-	return (1);
+	for (digit1 = 0x30; digit1 <= 0x39; digit1++)
+	{
+		for (digit2 = 0x30; digit2 <= 0x39; digit2++)
+		{
+			for (digit3 = 0x30; digit3 <= 0x39; digit3++)
+			{
+				if (digit1 < digit2 && digit2 < digit3)
+				{
+					int req1;
+					int req2;
+
+					putchar(digit1);
+					putchar(digit2);
+					putchar(digit3);
+					req1 = digit1 >= 0x30 && digit1 < 0x37;
+					req2 = digit2 >= 0x30 && digit2 <= 0x38;
+
+					if (req1 && req2 && digit3 <= 0x39)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
 }
 
